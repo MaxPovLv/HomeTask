257faee022e08e3863a1c1399e9b99555a0cd889
MaxPovLv <max.pov.lv@gmail.com>
2022-02-02 01:17:33 +0200

    Done

---
+++ js/lesson1/classwork/main.js
@@ -0,0 +1,62 @@
+{
+    let num = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    let result = num[0] + num[1] + num[2] + num[3] + num[4]
+        + num[5] + num[6] + num[7] + num[8] + num[9]
+    console.log(result)
+}
+{
+    let book = {
+        title: 'назва',
+        pages: 'кількість сторінок',
+        genre: 'жанр'
+    }
+}
+{
+    let book = {
+        title: 'назва',
+        pages: 'кількість сторінок',
+        genre: 'жанр',
+        authors: 'автори'
+    }
+}
+{
+    let books = [
+        book0 = {
+            title: 'назва',
+            pages: 'кількість сторінок',
+            genre: 'жанр',
+            authors: 'автори'
+        },
+        book1 = {
+            title: 'назва',
+            pages: 'кількість сторінок',
+            genre: 'жанр',
+            authors: 'автори'
+        },
+        book2 = {
+            title: 'назва',
+            pages: 'кількість сторінок',
+            genre: 'жанр',
+            authors: 'автори'
+        }
+    ]
+}
+{
+    let height = 23,
+        width = 10,
+        s = height * width;
+    console.log(s)
+}
+{
+    let heightC = 10,
+        dC = 4,
+        v = heightC * 3.14 * ((dC / 2) ** 2);
+    console.log(v)
+}
+{
+    let n = 3,
+        m = 4,
+        k = Math.sqrt((n ** 2) + (n ** 2));
+    console.log(k)
+}
+
---
+++ js/lesson1/homework/main.js
@@ -0,0 +1,170 @@
+{
+    let a = 'hello',
+        b = 'owu',
+        c = 'com',
+        d = 'ua',
+        e = 1,
+        f = 10,
+        g = 10,
+        j = 123,
+        k = 3.14,
+        l = 2.7,
+        m = 16,
+        n = true,
+        o = false;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log(e);
+    console.log(f);
+    console.log(g);
+    console.log(j);
+    console.log(k);
+    console.log(l);
+    console.log(m);
+    console.log(n);
+    console.log(o);
+
+    alert(a);
+    alert(b);
+    alert(c);
+    alert(d);
+    alert(e);
+    alert(f);
+    alert(g);
+    alert(j);
+    alert(k);
+    alert(l);
+    alert(m);
+    alert(n);
+    alert(o);
+
+    document.write(a);
+    document.write(b);
+    document.write(c);
+    document.write(d);
+    document.write(e);
+    document.write(f);
+    document.write(g);
+    document.write(j);
+    document.write(k);
+    document.write(l);
+    document.write(m);
+    document.write(n);
+    document.write(o);
+}
+{
+    let a = 1,
+        b = 2,
+        c = 3,
+        d = 4,
+        e = 5,
+        f = 6,
+        g = 7,
+        j = 8,
+        k = 9,
+        l = 10,
+        m = 11,
+        n = 12,
+        o = 13;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log(e);
+    console.log(f);
+    console.log(g);
+    console.log(j);
+    console.log(k);
+    console.log(l);
+    console.log(m);
+    console.log(n);
+    console.log(o);
+
+    alert(a);
+    alert(b);
+    alert(c);
+    alert(d);
+    alert(e);
+    alert(f);
+    alert(g);
+    alert(j);
+    alert(k);
+    alert(l);
+    alert(m);
+    alert(n);
+    alert(o);
+
+    document.write(a);
+    document.write(b);
+    document.write(c);
+    document.write(d);
+    document.write(e);
+    document.write(f);
+    document.write(g);
+    document.write(j);
+    document.write(k);
+    document.write(l);
+    document.write(m);
+    document.write(n);
+    document.write(o);
+}
+
+let firstName = 'Maksym',
+    middleName = 'Pavlovych',
+    lastName = 'Popovych',
+    age = 25;
+
+
+let person = {
+    firstName: 'Maksym',
+    middleName: 'Pavlovych',
+    lastName: 'Popovych'
+}
+
+
+let first = prompt(firstName);
+let second = prompt(middleName);
+let third = prompt(age);
+
+
+document.write('Вітаю' + firstName + middleName + '.' + 'Тобі' + age + 'років');
+
+{
+    let a = 100;
+    let b = '100';
+    let c = true;
+
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+}
+
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 >= 6);
+console.log(5 === 6);
+
+console.log(10 === 10);
+console.log(10 == 10);
+console.log(10 !== 10);
+console.log(10 != 10);
+console.log(10 > 10);
+
+console.log(123 === '123');
+console.log(123 == '123');
+
+{
+    let str = "20";
+    let a = 5;
+    document.write(str + a + "<br/>");
+    document.write(str - a + "<br/>");
+    document.write(str * "2" + "<br/>");
+    document.write(str / 2 + "<br/>");
+}
+
+// Перше 205, тому що 20 стрінга і додавання доставляє 5 рядом
+// А всі інші операції прораховує за юнікодом цифр
